5a597dfc6ec5f6b252a307f4806524ad595b52d4
Codemaniac6 <56360107+Codemaniac6@users.noreply.github.com>
2021-07-26 17:37:21 +0100

    user preference

--- mflix/db.py
+++ mflix/db.py
@@ -264,6 +264,7 @@ def get_movie(id):
 
         # TODO: Get Comments
         # Implement the required pipeline.
+        let = {}
         pipeline = [
             {
                 "$match": {
@@ -414,7 +415,7 @@ def add_user(name, email, hashedpw):
         # Insert a user with the "name", "email", and "password" fields.
         # TODO: Durable Writes
         # Use a more durable Write Concern for this operation.
-        db.users.insert_one({
+        db.users.with_options(write_concern=WriteConcern(w=2)).insert_one({
             "name": name,
             "email": email,
             "password": hashedpw
@@ -510,8 +511,8 @@ def update_prefs(email, prefs):
         # TODO: User preferences
         # Use the data in "prefs" to update the user's preferences.
         response = db.users.update_one(
-            { "some_field": "some_value" },
-            { "$set": { "some_other_field": "some_other_value" } }
+            { "email": email },
+            { "$set": { "preferences": prefs } }
         )
         if response.matched_count == 0:
             return {'error': 'no user found'}
