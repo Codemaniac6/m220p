1d3615f4e645c15c60c7d2ab742281d07be7b900
Codemaniac6 <56360107+Codemaniac6@users.noreply.github.com>
2021-07-27 06:21:31 +0100

    migration

--- mflix/db.py
+++ mflix/db.py
@@ -350,7 +350,13 @@ def add_comment(movie_id, user, comment, date):
     """
     # TODO: Create/Update Comments
     # Construct the comment document to be inserted into MongoDB.
-    comment_doc = { "some_field": "some_value" }
+    comment_doc = { 
+        "name": user.name,
+        "email": user.email,
+        "movie_id": ObjectId(movie_id),
+        "text": comment,
+        "date": date 
+        }
     return db.comments.insert_one(comment_doc)
 
 
@@ -364,8 +370,8 @@ def update_comment(comment_id, user_email, text, date):
     # Use the user_email and comment_id to select the proper comment, then
     # update the "text" and "date" of the selected comment.
     response = db.comments.update_one(
-        { "some_field": "some_value" },
-        { "$set": { "some_other_field": "some_other_value" } }
+        { "_id": ObjectId(comment_id), "email": user_email },
+        { "$set": { "text": text, "date": date } }
     )
 
     return response
@@ -386,7 +392,7 @@ def delete_comment(comment_id, user_email):
 
     # TODO: Delete Comments
     # Use the user_email and comment_id to delete the proper comment.
-    response = db.comments.delete_one( { "_id": ObjectId(comment_id) } )
+    response = db.comments.delete_one( { "_id": ObjectId(comment_id), 'email': user_email } )
     return response
 
 
@@ -557,10 +563,17 @@ def most_active_commenters():
     """
     # TODO: User Report
     # Return the 20 users who have commented the most on MFlix.
-    pipeline = []
+    pipeline = [
+        {
+            '$sortByCount' : '$email'
+        },
+        {
+            '$limit': 20
+        }
+    ]
 
-    rc = db.comments.read_concern # you may want to change this read concern!
-    comments = db.comments.with_options(read_concern=rc)
+    # you may want to change this read concern!
+    comments = db.comments.with_options(read_concern=ReadConcern(level='majority'))
     result = comments.aggregate(pipeline)
     return list(result)
 
